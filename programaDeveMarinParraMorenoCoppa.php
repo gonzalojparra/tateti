062db5af0c32dc19b08b802d070ab7072e2a7b06
gonzalojparra <gonzzaparra@gmail.com>
2021-11-18 21:52:47 -0300

    Modificado

--- programaDeveMarinParraMorenoCoppa.php
+++ programaDeveMarinParraMorenoCoppa.php
@@ -15,12 +15,14 @@ include_once("tateti.php");
 /***** DEFINICION DE FUNCIONES ********/
 /**************************************/
 
-/** Módulo para cargar juegos que se desarrollaron 
+/**
+ * Módulo para cargar juegos que se desarrollaron 
  * @param void
  * @return array
  */
 function cargarJuegos() {
 
+    //array $juegos
     $juegos[0] = [ 
         'jugadorCruz' => 'Emiliano',
         'jugadorCirculo' => 'Agustin',
@@ -112,31 +114,47 @@ function seleccionarOpcion() {
     echo "5) Mostrar resumen de Jugador\n";
     echo "6) Mostrar listado de juegos ordenado por jugador O\n";
     echo "7) Salir\n";
-    echo "Elija la opción a la que desea ingresar: ";
+    echo "Ingrese la opción en la que desea ingresar: \n";
     $numero = trim(fgets(STDIN));
-    while (!($numero >= 1 && $numero <= 7)) {
-        echo "El numero es invalido, debe ser un número entre el 1 y el 7\n";
-        echo "Vuelva a ingresar un número: ";
+    if (is_string($numero) && !($numero >= 1 && $numero <= 7)) {
+        echo "¡Opción inválida! Debe ser un número entre el 1 y 7\n";
+        echo "Vuelva a ingresar el número: ";
         $numero = trim(fgets(STDIN));
     }
     return $numero;	
 }
 
 /**
+ * Este módulo le solicita al usuario un número entre dos valores y si el número es inválido lo pide de vuelta
+ * @param int $valor1, $valor2
+ * @return int
+ */
+function validarNumero ($valor1, $valor2) {
+    do {
+        echo "Ingrese un numero entre " .$valor1. " y " .$valor2. ": ";
+        $numeroIngresado = trim(fgets(STDIN));
+        if (!(($valor1 <= $numeroIngresado) && ($numeroIngresado <= $valor2))) {
+            echo "El numero de juego no existe.\n";
+        }
+    } while (!(($valor1 <= $numeroIngresado) && ($numeroIngresado <= $valor2)));
+    return $numeroIngresado;
+}
+
+/**
 * Este módulo solicita un número y muestra datos del juego
-* @param int
-* @param array
+* @param int $numeroDeJuego
+* @param array $seleccionJuego
 * @return void
 */
 function mostrarJuego ($numeroDeJuego, $seleccionJuego) {
-    
-    echo "Ingrese el número de juego que desea visualizar: ";
+
+    /*echo "Ingrese el número de juego que desea visualizar: ";
     $numeroDeJuego=trim(fgets(STDIN));
     while (!($numeroDeJuego >= 0 && $numeroDeJuego <= 9)) {
         echo "El número es invalido, debe ser un número entre el 0 y 9\n";
         echo "Vuelva a ingresar un número: ";
         $numeroDeJuego = trim(fgets(STDIN));
-    }
+    }*/
 
     if ($seleccionJuego[$numeroDeJuego]["puntosCruz"] > $seleccionJuego[$numeroDeJuego]["puntosCirculo"]) {
         echo "******************************\n";
@@ -191,35 +209,41 @@ function agregarJuego($juegos, $unJuego) {
 
 //Inicialización de variables:
 
+//Se cargan los 10 juegos a la lista
+$listaDeJuegos = cargarJuegos();
 
 //Proceso:
+do {
+    $opcion = seleccionarOpcion();
+    switch ($opcion) {
+        case 1: 
+            //Jugar al tateti
+            $juegoNuevo = jugar();
+            imprimirResultado($juegoNuevo);
+            //Almacenar juego en la base de datos
+            $listaDeJuegos = agregarJuego($listaDeJuegos, $juegoNuevo);
+            break;
+        case 2:
+            //Mostrar un juego
+            if (count($listaDeJuegos) > 0) {
+                $valorUno = 0;
+                $valorDos = (count($listaDeJuegos)-1);
+                $numJuego = validarNumero($valorUno, $valorDos);
+                //Mostrar el juego pedido
+                mostrarJuego($numJuego, $listaDeJuegos);
+            } else {
+                echo "No hay ningún juego registrado.\n";
+            }
+            break;
+        case 3:
+            //completar qué secuencia de pasos ejecutar si el usuario elige la opción 3
+            break;
+        case 7:
+            //Muestra cartel de finalización de juego
+            echo "Juego finalizado.";
+    }
+ } while ($opcion != 7);
 
 //$juego = jugar();
 //print_r($juego);
 //imprimirResultado($juego);
-
-
-
-/* 
-do {
-   $opcion = ...;
-
-    
-    switch ($opcion) {
-        case 1: 
-            //completar qué secuencia de pasos ejecutar si el usuario elige la opción 1
-
-            break; (por cada case debe existir un break)
-        case 2: 
-            //completar qué secuencia de pasos ejecutar si el usuario elige la opción 2
-
-            break;
-        case 3: 
-            //completar qué secuencia de pasos ejecutar si el usuario elige la opción 3
-
-            break;
-        
-            //...
-    }
-} while ($opcion != X);
-*/
